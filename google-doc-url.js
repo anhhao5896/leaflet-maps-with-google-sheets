e9aecc4ae19e6a223d28a454a46b8aa9cc222cc0
anhhao5896 <48664350+anhhao5896@users.noreply.github.com>
2021-08-12 12:00:35 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_0gIstz3J4qTdHnVi8OP3InhCXcEx7gxhjt-Y3UFO8Y/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBH1487N8LiNxaazt6A4DcY_pfnNcB7XdI';
